58bf486aee200acc3bf786a69ee47f750670ba8f
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2024-06-11 21:38:09 +0800

    Initial Commit

---
+++ BonjourBrowser/BonjourBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  BonjourBrowserApp.swift
+//  BonjourBrowser
+//
+//  Created by Lok Shun Hung on 11/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct BonjourBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BonjourBrowser/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BonjourBrowser
+//
+//  Created by Lok Shun Hung on 11/6/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
