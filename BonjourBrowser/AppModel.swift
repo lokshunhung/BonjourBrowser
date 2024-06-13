3e7c2b0a1b136af9d3d991a6970cbeffbef5bc58
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2024-06-13 21:11:26 +0800

    NavigationStack State Persistence

--- BonjourBrowser/AppModel.swift
+++ BonjourBrowser/AppModel.swift
@@ -10,5 +10,55 @@ import Foundation
 import SwiftUI
 
 public final class AppModel: ObservableObject {
-    @Published public var path: NavigationPath = .init()
+    private let pathStorage: NavigationPathStorage
+    private var cancellables: Set<AnyCancellable> = []
+
+    @Published public var path: NavigationPath
+
+    public init(pathStorage: NavigationPathStorage = .live(.standard)) {
+        self.pathStorage = pathStorage
+        self.path = pathStorage.read()
+
+        self.$path
+            .debounce(for: .seconds(1.5), scheduler: DispatchQueue.main)
+            .sink { [pathStorage] path in
+                pathStorage.write(path)
+            }
+            .store(in: &cancellables)
+    }
+
+    public struct NavigationPathStorage {
+        public var read: () -> NavigationPath
+        public var write: (_ path: NavigationPath) -> ()
+
+        public init(
+            read: @escaping () -> NavigationPath,
+            write: @escaping (_ path: NavigationPath) -> ()
+        ) {
+            self.read = read
+            self.write = write
+        }
+    }
+}
+
+extension AppModel.NavigationPathStorage {
+    private static let key = "gnlok.BonjourBrowser.AppModel.path"
+
+    public static func live(_ userDefaults: UserDefaults = .standard) -> Self {
+        .init(
+            read: { () in
+                if let data = userDefaults.data(forKey: Self.key),
+                   let codable = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data) {
+                    return NavigationPath(codable)
+                }
+                return NavigationPath()
+            },
+            write: { path in
+                if let codable = path.codable,
+                   let data = try? JSONEncoder().encode(codable) {
+                    userDefaults.set(data, forKey: Self.key)
+                }
+            }
+        )
+    }
 }
